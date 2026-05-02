b2df9f0929b88d09a164a9dda8235c2c5c9e4797
NamPE286 <nambuihung654@gmail.com>
2026-05-03 04:04:27 +0700

    fix: random choice from candidate

--- mini-project/training/mlm.py
+++ mini-project/training/mlm.py
@@ -1,4 +1,5 @@
 import random
+
 import const.special_token as token
 from const.mlm import IGNORE_INDEX
 
@@ -6,16 +7,24 @@ from const.mlm import IGNORE_INDEX
 def create_mlm_sample(
     input_ids: list[int],
     vocab: dict[str, int],
-    mask_prob=0.15,
+    mask_prob: float = 0.15,
 ) -> tuple[list[int], list[int]]:
-    masked_ids, labels = list(input_ids), [IGNORE_INDEX] * len(input_ids)
-    vocab_ids = list(vocab.values())
-    mask_id = vocab.get("[MASK]", 0)
+    masked_ids = list(input_ids)
+    labels = [IGNORE_INDEX] * len(input_ids)
+
+    mask_id = vocab[token.MASK]
+
     special_token_ids = {
         vocab[special]
         for special in token.SPECIAL_TOKENS
         if special in vocab
     }
+
+    candidate_vocab_ids = [
+        token_id
+        for token_id in vocab.values()
+        if token_id not in special_token_ids
+    ]
 
     for i, token_id in enumerate(input_ids):
         if token_id in special_token_ids:
@@ -25,11 +34,14 @@ def create_mlm_sample(
             continue
 
         labels[i] = token_id
+
         rand = random.random()
-        masked_ids[i] = (
-            mask_id
-            if rand < 0.8
-            else random.choice(vocab_ids) if rand < 0.9 else token_id
-        )
+
+        if rand < 0.8:
+            masked_ids[i] = mask_id
+        elif rand < 0.9:
+            masked_ids[i] = random.choice(candidate_vocab_ids)
+        else:
+            masked_ids[i] = token_id
 
     return masked_ids, labels
