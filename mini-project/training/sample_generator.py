28cb881d12bfeb8aae9b981fdfa7af830e3734fa
NamPE286 <nambuihung654@gmail.com>
2026-05-02 18:24:32 +0700

    feat: sample generator

--- mini-project/training/sample_generator.py
+++ mini-project/training/sample_generator.py
@@ -1,6 +1,24 @@
+import random
+
+IGNORE_INDEX = -100
+
 def create_mlm_sample(
-    input_ids: tuple[list[int], list[int]],
+    input_ids: list[int],
     vocab: dict[str, int],
     mask_prob=0.15,
-):
-    pass
+) -> tuple[list[int], list[int]]:
+    masked_ids, labels = list(input_ids), [IGNORE_INDEX] * len(input_ids)
+    vocab_ids = list(vocab.values())
+    mask_id = vocab.get("[MASK]", 0)
+
+    for i, token_id in enumerate(input_ids):
+        if random.random() < mask_prob:
+            labels[i] = token_id 
+            rand = random.random()
+            masked_ids[i] = (
+                mask_id
+                if rand < 0.8
+                else random.choice(vocab_ids) if rand < 0.9 else token_id
+            )
+
+    return masked_ids, labels
