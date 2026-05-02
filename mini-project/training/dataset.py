a7a22bfc16a974143f9d8a712f57184519acf4a2
NamPE286 <nambuihung654@gmail.com>
2026-05-03 04:21:58 +0700

    refactor: migrate to pytorch

--- mini-project/training/dataset.py
+++ mini-project/training/dataset.py
@@ -1,10 +1,14 @@
 from __future__ import annotations
 
+import torch
+from torch import Tensor
+from torch.utils.data import Dataset
+
 from tokenizer.encoder import encode_tokens
 from training.mlm import create_mlm_sample
 
 
-class MLMDataset:
+class MLMDataset(Dataset[dict[str, Tensor]]):
     tokenized_corpus: list[list[str]]
     vocab: dict[str, int]
     max_seq_len: int
@@ -22,7 +26,7 @@ class MLMDataset:
     def __len__(self) -> int:
         return len(self.tokenized_corpus)
 
-    def __getitem__(self, index: int) -> dict[str, list[int]]:
+    def __getitem__(self, index: int) -> dict[str, Tensor]:
         tokens = self.tokenized_corpus[index]
 
         input_ids, attention_mask = encode_tokens(
@@ -37,7 +41,7 @@ class MLMDataset:
         )
 
         return {
-            "input_ids": masked_ids,
-            "attention_mask": attention_mask,
-            "labels": labels,
+            "input_ids": torch.tensor(masked_ids, dtype=torch.long),
+            "attention_mask": torch.tensor(attention_mask, dtype=torch.long),
+            "labels": torch.tensor(labels, dtype=torch.long),
         }
