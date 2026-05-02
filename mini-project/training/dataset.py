8f51c61b6442fd85b9af96aad0bd6d76fd77313e
NamPE286 <nambuihung654@gmail.com>
2026-05-02 18:42:54 +0700

    feat: MLMDataset

---
+++ mini-project/training/dataset.py
@@ -0,0 +1,43 @@
+from __future__ import annotations
+
+from tokenizer.encoder import encode_tokens
+from training.mlm import create_mlm_sample
+
+
+class MLMDataset:
+    tokenized_corpus: list[list[str]]
+    vocab: dict[str, int]
+    max_seq_len: int
+
+    def __init__(
+        self,
+        tokenized_corpus: list[list[str]],
+        vocab: dict[str, int],
+        max_seq_len: int = 128,
+    ) -> None:
+        self.tokenized_corpus = tokenized_corpus
+        self.vocab = vocab
+        self.max_seq_len = max_seq_len
+
+    def __len__(self) -> int:
+        return len(self.tokenized_corpus)
+
+    def __getitem__(self, index: int) -> dict[str, list[int]]:
+        tokens = self.tokenized_corpus[index]
+
+        input_ids, attention_mask = encode_tokens(
+            tokens,
+            self.vocab,
+            self.max_seq_len,
+        )
+
+        masked_ids, labels = create_mlm_sample(
+            input_ids,
+            self.vocab,
+        )
+
+        return {
+            "input_ids": masked_ids,
+            "attention_mask": attention_mask,
+            "labels": labels,
+        }
